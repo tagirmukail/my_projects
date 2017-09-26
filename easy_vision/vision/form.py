263ae92d33131353d3ce98331ed05577a974f07e
TagirMukailov <mukailov1991@gmail.com>
2017-09-26 18:58:29 +0300

    удален ненужный каталог

--- easy_vision/vision/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    sender = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
-    copy = forms.BooleanField(required=False)
